7e0a9349968f69c343d67a37fa1f2b9b23c10781
tubagus.fakhridhiya@gmail.com <tubagus.fakhridhiya@gmail.com>
2023-11-13 11:03:24 +0700

    update konfigurasi-firebase.js

--- konfigurasi-firebase.js
+++ konfigurasi-firebase.js
@@ -1,9 +1,15 @@
 export const konfigurasiFirebase = {
-    apiKey: "AIzaSyCjXlgysJkN-2s3Gu0forgp7as5-9NqCkI",
-    authDomain: "pasar-b04a7.firebaseapp.com",
-    projectId: "pasar-b04a7",
-    storageBucket: "pasar-b04a7.appspot.com",
-    messagingSenderId: "508470916587",
-    appId: "1:508470916587:web:460e9a1612e92b712e15ae",
-    measurementId: "G-33T7CQCWBX"
+   // apiKey: "AIzaSyCjXlgysJkN-2s3Gu0forgp7as5-9NqCkI",
+//    authDomain: "pasar-b04a7.firebaseapp.com",
+  //  projectId: "pasar-b04a7",
+//    storageBucket: "pasar-b04a7.appspot.com",
+//    messagingSenderId: "508470916587",
+//    appId: "1:508470916587:web:460e9a1612e92b712e15ae",
+//    measurementId: "G-33T7CQCWBX"
+  apiKey: "AIzaSyCDE9RCp2gUIyOG8hUEv5un0JlXqHWowkA",
+  authDomain: "crud-6df8e.firebaseapp.com",
+  projectId: "crud-6df8e",
+  storageBucket: "crud-6df8e.appspot.com",
+  messagingSenderId: "22846946310",
+  appId: "1:22846946310:web:f393dbd1ff1ad9d61f1787
   };
